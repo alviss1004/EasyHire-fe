42b665e0baf26bd9bb16aaff47fbe7eb5deb8493
Binh Nguyen <104481230+alviss1004@users.noreply.github.com>
2023-02-28 18:13:05 +0700

    Update config.js

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  "https://helpless-governor-production.up.railway.app/api";
+  "https://easyhire-be-production.up.railway.app/api";
 export const JOBS_PER_PAGE = 10;
 export const FREELANCERS_PER_PAGE = 10;
 export const COMMENTS_PER_PAGE = 5;
