5ed7237f4993eb664a17bc416df35f4f56f862d8
Binh Nguyen <104481230+alviss1004@users.noreply.github.com>
2023-03-30 10:37:06 +0700

    Update apiService.js

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./config";
 
 const apiService = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://confused-quartz-production.up.railway.app/api",
 });
 
 apiService.interceptors.request.use(
